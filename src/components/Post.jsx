ed3492ea148f971ee829fbb7f57b76c448f0f373
henrikesilva <henrikelima.0502@gmail.com>
2023-05-29 12:02:39 -0300

    Criação e estilização do componente de 'Post'

---
+++ src/components/Post.jsx
@@ -0,0 +1,30 @@
+import styles from './Post.module.css'
+
+export function Post() {
+    return (
+        <article className={styles.post}>
+            <header>
+                <div className={styles.author}>
+                    <img className={styles.avatar} src="https://github.com/henrikesilva.png" />
+                    <div className={styles.authorInfo}>
+                        <strong>Henrique Lima</strong>
+                        <span>Fullstack dev</span>
+                    </div>
+                </div>
+
+                <time title="29 de Maio às 08:13" dateTime="2023-05-29 08:13:25">Públicado há 1h</time>
+            </header>
+
+            <div className={styles.content}>
+                <p>Fala galeraa 👋</p>
+                <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀 </p>
+                <p>👉{' '}<a href="#">jane.design/doctorcare</a></p>
+                <p>
+                    <a href="#">#novoprojeto</a>{' '}
+                    <a href="#">#nlw</a>{' '}
+                    <a href="#">#rocketseat</a>
+                </p>
+            </div>
+        </article>
+    )
+}
