77f3f0b4ab38455a41c9fdb5d0c182f8d24d177e
henrikesilva <henrikelima.0502@gmail.com>
2023-05-29 10:49:14 -0300

    Criação do layout inicial com variaveis e design padrão; criação do header da aplicação

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import { Post } from "./Post"
+import { Header } from "./components/Header";
+
+import './global.css';
 
 export function App() {
 
   return (
     <div>
+      <Header />
+
       <Post 
         author="Henrique Lima"
         content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, commodi dolor. Autem in nobis cumque? Deserunt quia culpa dolorem odio deleniti. Eos quidem culpa, earum ex nemo necessitatibus non quae."
